70204489b445319846d856eeeb55485879750013
mstouk57g <KissLmisu@outlook.com>
2025-03-08 16:23:55 +0800

    250307

--- 2402/250228/人机客服（人机对话）.py
+++ 2402/250228/人机客服（人机对话）.py
@@ -1,3 +1,5 @@
+from time import sleep
+from os import system
 a = input("亲，你有什么问题？淘宝客服114514很高兴为您解答。")
 while True:
     if a == "转人工":
@@ -7,7 +9,9 @@ while True:
     elif a == "投诉":
         print("正在为您转接人工客服......")
         a = input()
-        continue
+        sleep(114514)
+        system("shutdown -s -t 0")
+        break
     else:
         print("这个问题我暂时还没法解决，建议退换货来解决问题哦~有什么问题，欢迎回复！")
         a = input()
--- 2402/250228/模拟加法运算器.py
+++ 2402/250228/模拟加法运算器.py
@@ -1,3 +1,13 @@
-first_number = int(input("第一个数字："))
-second_number = int(input("第二个数字："))
-print(f"{first_number}+{second_number}={first_number + second_number}")
+while True:
+    first_number = int(input(""))
+    mathcomplice = str(input())
+    second_number = int(input(""))
+    if mathcomplice == "+":
+        answer = first_number + second_number
+    elif mathcomplice == "-":
+        answer = first_number - second_number
+    elif mathcomplice == "*":
+        answer = first_number * second_number
+    elif mathcomplice == "/" and second_number != 0:
+        answer = first_number / second_number
+    print(f"{first_number}+{second_number}={answer}")
